2e0ed9455ccb011c1794b88589064b57c11fdb84
Mang <mangemmanuel07@gmail.com>
2023-03-24 19:43:18 +0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("This is really cool!");
+//   });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    return document.getElementById('text').textContent = "This is really cool!";
+    
+  });
+
+   
+
